519196a122030e0788724b2c92bc86d965aa080b
Bikash Shaw <1985.bikash.shaw@gmail.com>
2021-12-05 19:07:22 +0530

    initial commit

---
+++ apps/online-dukaan/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bikash-software-pvt-ltd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'online-dukaan';
+}
